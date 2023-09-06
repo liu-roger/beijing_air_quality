055271a77ec6c3b45e0509fc2d489b1831133afd
liu-roger <rogerliu80@gmail.com>
2023-09-07 03:34:35 +0800

    global

--- global.R
+++ global.R
@@ -11,4 +11,5 @@ library(readr)
 #data source
 # https://archive.ics.uci.edu/dataset/501/beijing+multi+site+air+quality+data
 # https://www.apple.com/startpage/
-all_stations = read_csv('./all_stations.csv')
+all_stations = read_csv('./datasets/all_stations.csv')
+all_stations_with_aqi = read_csv('./datasets/all_stations_with_aqi.csv')
