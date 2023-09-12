e4b88ebbf217dd1b5cc08954eae3f3ab8a76bcb4
liu-roger <rogerliu80@gmail.com>
2023-09-13 00:50:29 +0800

    rearranged pages

--- ui.R
+++ ui.R
@@ -21,137 +21,24 @@ navbarPage(
     hr()  # Horizontal line for separation
   ),
   setBackgroundColor("white"),
-  tabPanel(
-    h4("Heat Map"),
-    fluidRow(
-      fluidRow(style = "border: 1px solid #E54B4B;",
-        div(
-          style = "text-align: center;",
-          column(12,
-            h4('Map of Beijing and Data Collection Stations'),
-          ),
-        ),
-        column(12,
-          leafletOutput('beijing_map')
-        )
-      ),
-      fluidRow(style = "border: 1px solid #E54B4B;",
-        column(6,
-          div(
-            selectizeInput(inputId = "heatmap_particulate_selection",
-              label = "Particulate Selection",
-              choices = colnames(all_stations)[c(10:15)]
-            ),
-          style = "display: flex; flex-direction: column; align-items: center; justify-content: center;",   
-          )
-        ),
-        column(6,
-          div(
-            dateInput('heatmap_date_input',
-              label = 'Date to Analyze: yyyy-mm-dd',
-              value = max(all_stations$date),
-              min =  min(all_stations$date), max = max(all_stations$date)
-            ),
-          style = "display: flex; flex-direction: column; align-items: center; justify-content: center;"
-          )
-        ),
-      ),
-      fluidRow(style = "border: 1px solid #E54B4B;",
-        column(12,"datatable for the heatmap",
-          #create the datatable
-          DT::dataTableOutput('heatmap_dt')
-        )
-      )
-    )
-  ),
-  
-  tabPanel(
-    h4("Line Graphs"),
-    fluidRow(style = "border: 1px solid #E54B4B;",
-      fluidRow(style = "border: 1px solid #E54B4B;",
-        column(5,offset = 1,
-          selectizeInput(inputId = "station_name1_line_graph",
-            label = "First Station Name",
-            choices = unique(all_stations$station)
-          ),
-          
-          selectizeInput(inputId = "station_name2_line_graph",
-            label = "Second Station Name",
-            choices = unique(all_stations$station),
-            selected = unique(all_stations$station)[2]
-          ),
-          selectizeInput(inputId = "line_graph_particulate_selection",
-            label = "Particulate Selection",
-            choices = colnames(all_stations)[c(10:19,21)]
-          ),
-        ),
-        column(5, offset = 1,
-          dateRangeInput('dateRangeLineGraph',
-            label = 'Date range input: yyyy-mm-dd',
-            start = min(all_stations$date), end = max(all_stations$date),
-            min =  min(all_stations$date), max = max(all_stations$date)
-          ),
-            
-        )
-      ),
-    ),
-    tabsetPanel(
-      tabPanel('Mean',
-        plotOutput('meanParticulatesLineGraph'),
-        fluidRow(
-          column(6, verbatimTextOutput("station_1_name_mean"),
-            DT::dataTableOutput('station_1_dt_mean')
-          ),
-          column(6, verbatimTextOutput("station_2_name_mean"),
-            DT::dataTableOutput('station_2_dt_mean')
-          )
-        )
-      ),
-      tabPanel('Maximum',
-        plotOutput('maxParticulatesLineGraph'),
-        fluidRow(
-          column(6, verbatimTextOutput("station_1_name_max"),
-                 DT::dataTableOutput('station_1_dt_max')
-          ),
-          column(6, verbatimTextOutput("station_2_name_max"),
-                 DT::dataTableOutput('station_2_dt_max')
-          )
-        )
-      ),
-      tabPanel('Minimum',
-        plotOutput('minParticulatesLineGraph'),
-        fluidRow(
-          column(6, verbatimTextOutput("station_1_name_min"),
-                 DT::dataTableOutput('station_1_dt_min')
-          ),
-          column(6, verbatimTextOutput("station_2_name_min"),
-                 DT::dataTableOutput('station_2_dt_min')
-          )
-        )
-      )
-    )
-  ),
+
   
   tabPanel(
     h4("Time Series Analysis"),
     tabsetPanel(
-      tabPanel('Daily',
-        selectizeInput(inputId = "daily_particulate_selection",
-          label = "Particulate Selection",
-          choices = colnames(all_stations)[c(10:19,21)]
-        ),
-        dateInput('daily_date_input',
-          label = 'Date to Analyze: yyyy-mm-dd',
-          value = max(all_stations$date),
-          min =  min(all_stations$date), max = max(all_stations$date)
-        ),
-        plotOutput('daily_particulate_analysis'),
-        fluidRow(
-          column(12, verbatimTextOutput("daily_dt_name"),
-            DT::dataTableOutput('mean_daily_particulate')
-          )
-        )
+      tabPanel('Aggregate Analysis',
+               selectizeInput(inputId = "aggregate_particulate_selection",
+                              label = "Particulate Selection",
+                              choices = colnames(all_stations)[c(10:19,21)]
+               ),
+               plotOutput('aggregate_particulate_analysis'),
+               fluidRow(
+                 column(12, verbatimTextOutput("aggregate_dt_name"),
+                        DT::dataTableOutput('aggregate_dt')
+                 ),
+               )
       ),
+      
       tabPanel('Monthly and Yearly',
         selectizeInput(inputId = "monthly_particulate_selection",
           label = "Particulate Selection",
@@ -179,20 +66,142 @@ navbarPage(
           )
         )
       ),
-      tabPanel('Aggregate Analysis',
-        selectizeInput(inputId = "aggregate_particulate_selection",
-          label = "Particulate Selection",
-          choices = colnames(all_stations)[c(10:19,21)]
-        ),
-      plotOutput('aggregate_particulate_analysis'),
-      fluidRow(
-        column(12, verbatimTextOutput("aggregate_dt_name"),
-          DT::dataTableOutput('aggregate_dt')
-        ),
-      )
+      
+      tabPanel('Daily',
+               selectizeInput(inputId = "daily_particulate_selection",
+                              label = "Particulate Selection",
+                              choices = colnames(all_stations)[c(10:19,21)]
+               ),
+               dateInput('daily_date_input',
+                         label = 'Date to Analyze: yyyy-mm-dd',
+                         value = max(all_stations$date),
+                         min =  min(all_stations$date), max = max(all_stations$date)
+               ),
+               plotOutput('daily_particulate_analysis'),
+               fluidRow(
+                 column(12, verbatimTextOutput("daily_dt_name"),
+                        DT::dataTableOutput('mean_daily_particulate')
+                 )
+               )
+      ),
+      
+    )
+  ),
+  
+  tabPanel(
+    h4("Heat Map"),
+    fluidRow(
+      fluidRow(style = "border: 1px solid #E54B4B;",
+               div(
+                 style = "text-align: center;",
+                 column(12,
+                        h4('Map of Beijing and Data Collection Stations'),
+                 ),
+               ),
+               column(12,
+                      leafletOutput('beijing_map')
+               )
+      ),
+      fluidRow(style = "border: 1px solid #E54B4B;",
+               column(6,
+                      div(
+                        selectizeInput(inputId = "heatmap_particulate_selection",
+                                       label = "Particulate Selection",
+                                       choices = colnames(all_stations)[c(10:15)]
+                        ),
+                        style = "display: flex; flex-direction: column; align-items: center; justify-content: center;",   
+                      )
+               ),
+               column(6,
+                      div(
+                        dateInput('heatmap_date_input',
+                                  label = 'Date to Analyze: yyyy-mm-dd',
+                                  value = max(all_stations$date),
+                                  min =  min(all_stations$date), max = max(all_stations$date)
+                        ),
+                        style = "display: flex; flex-direction: column; align-items: center; justify-content: center;"
+                      )
+               ),
+      ),
+      fluidRow(style = "border: 1px solid #E54B4B;",
+               column(12,"datatable for the heatmap",
+                      #create the datatable
+                      DT::dataTableOutput('heatmap_dt')
+               )
       )
     )
-  )
+  ),
+  
+  tabPanel(
+    h4("Station Comparison"),
+    fluidRow(style = "border: 1px solid #E54B4B;",
+             fluidRow(style = "border: 1px solid #E54B4B;",
+                      column(5,offset = 1,
+                             selectizeInput(inputId = "station_name1_line_graph",
+                                            label = "First Station Name",
+                                            choices = unique(all_stations$station)
+                             ),
+                             
+                             selectizeInput(inputId = "station_name2_line_graph",
+                                            label = "Second Station Name",
+                                            choices = unique(all_stations$station),
+                                            selected = unique(all_stations$station)[2]
+                             ),
+                             selectizeInput(inputId = "line_graph_particulate_selection",
+                                            label = "Particulate Selection",
+                                            choices = colnames(all_stations)[c(10:19,21)]
+                             ),
+                      ),
+                      column(5, offset = 1,
+                             dateRangeInput('dateRangeLineGraph',
+                                            label = 'Date range input: yyyy-mm-dd',
+                                            start = min(all_stations$date), end = max(all_stations$date),
+                                            min =  min(all_stations$date), max = max(all_stations$date)
+                             ),
+                             
+                      )
+             ),
+    ),
+    tabsetPanel(
+      tabPanel('Mean',
+               plotOutput('meanParticulatesLineGraph'),
+               fluidRow(
+                 column(6, verbatimTextOutput("station_1_name_mean"),
+                        DT::dataTableOutput('station_1_dt_mean')
+                 ),
+                 column(6, verbatimTextOutput("station_2_name_mean"),
+                        DT::dataTableOutput('station_2_dt_mean')
+                 )
+               )
+      ),
+      tabPanel('Maximum',
+               plotOutput('maxParticulatesLineGraph'),
+               fluidRow(
+                 column(6, verbatimTextOutput("station_1_name_max"),
+                        DT::dataTableOutput('station_1_dt_max')
+                 ),
+                 column(6, verbatimTextOutput("station_2_name_max"),
+                        DT::dataTableOutput('station_2_dt_max')
+                 )
+               )
+      ),
+      tabPanel('Minimum',
+               plotOutput('minParticulatesLineGraph'),
+               fluidRow(
+                 column(6, verbatimTextOutput("station_1_name_min"),
+                        DT::dataTableOutput('station_1_dt_min')
+                 ),
+                 column(6, verbatimTextOutput("station_2_name_min"),
+                        DT::dataTableOutput('station_2_dt_min')
+                 )
+               )
+      )
+    )
+  ),
+  
+  
+  
+  
 )
 
   
